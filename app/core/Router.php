e8e56fbcfd5a8da4ed6ca09c6660ddfa141d414f
Ikuyo-v2 <d1417266@gmail.com>
2026-01-27 14:29:00 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -6,7 +6,24 @@ class Router
 
     public function run()
     {
-        echo "Router is running";
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        if ($method == 'GET' && $uri == '/students') {
+        echo '<h1>Students Listss</h1>';
+        echo '<p>Menambpikan Daftar siswa</p>';
+        return;
+        }
+
+          if ($method == 'GET' && $uri == '/students/create') {
+        echo '<h1>Add Students </h1>';
+        echo '<p>Menambpikan form tambah siswa</p>';
+        return;
+        }
+
+
+        http_response_code(404);
+        echo '<h1>404 Not Found</h1>';
     }
 }
 
